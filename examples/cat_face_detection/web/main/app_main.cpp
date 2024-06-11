a45dfa54caad58de7598704e3b3884ae3f8390b8
Lee Jackson <lee.jackson@arducam.com>
2024-06-11 16:32:33 +0800

    add arducam_mega_ccm demo

--- examples/cat_face_detection/web/main/app_main.cpp
+++ examples/cat_face_detection/web/main/app_main.cpp
@@ -14,7 +14,7 @@ extern "C" void app_main()
     xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
     xQueueHttpFrame = xQueueCreate(2, sizeof(camera_fb_t *));
 
-    register_camera(PIXFORMAT_RGB565, FRAMESIZE_QVGA, 2, xQueueAIFrame);
+    register_camera(PIXFORMAT_JPEG, FRAMESIZE_QVGA, 2, xQueueAIFrame);
     app_mdns_main();
     register_cat_face_detection(xQueueAIFrame, NULL, NULL, xQueueHttpFrame);
     register_httpd(xQueueHttpFrame, NULL, true);
